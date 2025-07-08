21ec0ed4e4b4d0b2eba962368d9a36f579a3ec71
Guilhem7 <guilhemrioux@gmail.com>
2025-07-08 16:57:26 +0200

    Push repo

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+#[derive(Debug)]
+pub struct Color;
+
+impl Color {
+    pub const RESET: &'static str = "\x1b[0m";
+    pub const BOLD: &'static str = "\x1b[1m";
+    pub const RED: &'static str = "\x1b[31m";
+    pub const GREEN: &'static str = "\x1b[32m";
+    pub const YELLOW: &'static str = "\x1b[33m";
+    pub const BLUE: &'static str = "\x1b[34m";
+    pub const MAGENTA: &'static str = "\x1b[35m";
+    pub const CYAN: &'static str = "\x1b[36m";
+    pub const WHITE: &'static str = "\x1b[37m";
+
+    pub fn wrap(text: &str, color: &str) -> String {
+        format!("{}{}{}", color, text, Self::RESET)
+    }
+
+    pub fn rgb(r: u8, g: u8, b: u8) -> String {
+        format!("\x1b[38;5;{};{};{}m", r, g, b)
+    }
+}
+
+#[macro_export]
+macro_rules! msg {
+    ( $( $x:tt )* ) => {
+        {
+            println!("{} {}", Color::wrap("[+]", Color::GREEN), format!($($x)*));
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! err {
+    ( $( $x:tt )* ) => {
+        {
+            println!("{} {}", Color::wrap("[-]", Color::RED), format!($($x)*));
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! log {
+    ( $( $x:tt )* ) => {
+        {
+            println!("{} {}", Color::wrap("[-]", &format!("{}{}", Color::BOLD, Color::YELLOW)), format!($($x)*));
+        }
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,122 @@
+#![allow(dead_code, unused_variables, unused_imports)]
+mod owa;
+
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Write};
+use std::sync::{Arc, Mutex};
+use owa_enum::{Color, err, log, msg};
+use owa::Owa;
+use reqwest::blocking::{Client};
+use rayon::ThreadPoolBuilder;
+use rayon::prelude::*;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Target to attack
+    #[arg(short, long)]
+    target: String,
+
+    /// Username to check or file containing usernames
+    #[arg(short, long)]
+    user: String,
+
+    /// Password to use for authentication
+    #[arg(short, long, default_value_t = String::from("Azerty@123"))]
+    password: String,
+
+    /// Target domain (optional) to connect with
+    #[arg(short, long)]
+    domain: Option<String>,
+
+    /// Timeout to use for considering user does not exists
+    #[arg(long, default_value_t = 3)]
+    timeout: u64,
+
+    /// The number of thread to use
+    #[arg(long, default_value_t = 4)]
+    threads_number: usize,
+
+    /// Output valid users to a file
+    #[arg(short, long)]
+    output: Option<String>,
+}
+
+fn get_users(path: &str) -> io::Result<Vec<String>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    reader.lines().collect()
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let pool = ThreadPoolBuilder::new()
+                .num_threads(args.threads_number)
+                .build()
+                .expect("Failed to build thread pool");
+    msg!("Using {} threads", Color::wrap(args.threads_number.to_string().as_str(), Color::YELLOW));
+
+    let mut target = args.target;
+    let user = &args.user;
+
+    let mut users: Vec<String> = vec![(&user).to_string()];
+
+    if !target.starts_with("http") {
+        target = format!("https://{}", target);
+    }
+
+    msg!("Attacking {}", Color::wrap(&target, Color::BOLD));
+
+    if let Ok(user_list) = get_users(user) {
+        users = user_list;
+        msg!("Checking on {} users", users.len());
+    } else {
+        msg!("Checking on single user: {}", Color::wrap(&users[0], Color::BOLD));
+    }
+
+    let client = Client::builder().danger_accept_invalid_certs(true)
+                                  .timeout(std::time::Duration::from_secs(args.timeout))
+                                  .redirect(reqwest::redirect::Policy::none())
+                                  .build().expect("Help, cannot create a HTTP Client");
+
+    let mut owa_enumerator = owa::Owa::new(client, target, args.domain);
+    match owa_enumerator.get_domain_name(){
+        Ok(()) => {
+            msg!("Domain name {}", Color::wrap(&owa_enumerator.get_domain(), Color::CYAN));
+        }
+        Err(e) => {
+            err!("Got {}", e);
+            return ();
+        }
+    }
+
+    let buffer = Arc::new(Mutex::new(String::new()));
+    pool.install(|| {
+        users.par_iter().for_each( |username| {
+            match owa_enumerator.user_exists(&username, args.password.as_ref()) {
+                owa::OwaResult::PasswordValid => {
+                    msg!("User {}:{} is valid", Color::wrap(&username, Color::CYAN), Color::wrap(args.password.as_ref(), Color::CYAN));
+                    let mut buf = buffer.lock().unwrap();
+                    buf.push_str(&format!("{}:{}", username, args.password));
+                }
+                owa::OwaResult::UserExists => {
+                    msg!("User {} exists", Color::wrap(&username, Color::CYAN));
+                    let mut buf = buffer.lock().unwrap();
+                    buf.push_str(&username);
+                }
+                owa::OwaResult::UserNotFound => err!("User {} does not exists", Color::wrap(&username, Color::BOLD)),
+            }
+        })
+    });
+
+    let final_result = buffer.lock().unwrap();
+    if let Some(output) = args.output {
+        if ! final_result.is_empty(){
+            log!("Writting result to: {}", output);
+            let mut file = File::create(output).expect("Failed to create file");
+            file.write_all(final_result.as_bytes()).expect("Could not write result");
+        }
+    }
+}
